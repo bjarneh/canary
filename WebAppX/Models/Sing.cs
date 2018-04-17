05af079fbddde4d16d3d9f22039b375de9dcfa9a
bjarne holen <bjarneholen@gmail.com>
2018-04-17 19:40:56 +0200

    Added wireframe

---
+++ WebAppX/Models/Sing.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebAppX.Models
+{
+    public class Sing{
+    {
+        public String Id { get; set; }
+        public DateTime Last { get; set; }
+        public DateTime Next { get; set; }
+    }
+}
---
+++ WebAppX/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Http;
+
+namespace WebAppX
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Use static files inside wwwroot
+            app.UseStaticFiles();
+            // Directory listing
+            app.UseDirectoryBrowser();
+
+            // Explicit add Route and handler
+            var routeBuilder = new RouteBuilder(app);
+
+            routeBuilder.MapGet("hello/{name}", context =>
+            {
+                var name = context.GetRouteValue("name");
+                // This is the route handler when HTTP GET "hello/<anything>"  matches
+                // To match HTTP GET "hello/<anything>/<anything>,
+                // use routeBuilder.MapGet("hello/{*name}"
+                return context.Response.WriteAsync($"Hi, {name}");
+            });
+
+            var routes = routeBuilder.Build();
+            app.UseRouter(routes);
+
+            // Extracts routes from the Attribute stuff inside the Controllers I think..
+            app.UseMvc();
+        }
+    }
+}
