225d39a31a0254566a27a43d8b5d8c6e077450c6
Bjarne Holen <bjarneh@ifi.uio.no>
2020-02-17 12:21:36 +0100

    clearTimeout on destroy and reload

---
+++ ping.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+while true; do
+        now=$(date);
+        wget -q --method=POST http://localhost:5000/api/canary/resman.dns/7/s -O /dev/null;
+        echo "POST api/canary/resman.dns/7/s | ${now}";
+        wget -q --method=POST http://localhost:5000/api/canary/resman.vpn/5/s -O /dev/null;
+        echo "POST api/canary/resman.vpn/7/s | ${now}";
+        sleep 4;
+done;
