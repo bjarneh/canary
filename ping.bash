c171b528311bcec092a0868e5f8eb93b0080c598
Bjarne Holen <bjarneh@ifi.uio.no>
2020-02-25 09:57:59 +0100

    set -e on bash script

--- ping.bash
+++ ping.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -e
+
 while true; do
         now=$(date);
         wget -q --method=POST http://localhost:5000/api/canary/resman.dns/7/s -O /dev/null;
         echo "POST api/canary/resman.dns/7/s | ${now}";
         wget -q --method=POST http://localhost:5000/api/canary/resman.vpn/5/s -O /dev/null;
-        echo "POST api/canary/resman.vpn/7/s | ${now}";
+        echo "POST api/canary/resman.vpn/5/s | ${now}";
         sleep 4;
 done;
